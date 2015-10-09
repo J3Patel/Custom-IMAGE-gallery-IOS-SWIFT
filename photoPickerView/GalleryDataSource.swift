0948510c6faf9cbb2c733ce291d06360b9fef8d2
j3patel <jatinpatel101@gmail.com>
2015-10-09 12:28:31 +0530

    first commit

---
+++ photoPickerView/GalleryDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  GalleryDataSource.swift
+//  photoPickerView
+//
+//  Created by RajaSoftwareLabs on 09/10/15.
+//  Copyright © 2015 jatin. All rights reserved.
+//
+
+import Foundation
---
+++ photoPickerView/PhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.swift
+//  photoPickerView
+//
+//  Created by RajaSoftwareLabs on 09/10/15.
+//  Copyright © 2015 jatin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
