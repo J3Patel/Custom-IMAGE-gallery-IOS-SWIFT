3b2d6a79e23d646b04fc6bffcff3de6254f9a8f4
Nikhil Dhumal <nikhil.dhumal@tudip.nl>
2015-10-10 12:41:37 +0530

    added albums

--- photoPickerView/GalleryDataSource.swift
+++ photoPickerView/GalleryDataSource.swift
@@ -11,44 +11,73 @@ import Photos
 import UIKit
 
 public class GalleryDataSource {
-//  let assetsLibrary = ALAssetsLibrary()
-  var photoAssets: PHFetchResult?
-  var images: [UIImage] = [UIImage]()
-
-  init() {
-    authorizeForAccess { () -> Void in
-      //success
-      self.getAllImagesFromGallery()
-
-    }
-  }
-
-  func authorizeForAccess(allowed:() -> Void) {
-    if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.NotDetermined {
-      PHPhotoLibrary.requestAuthorization({ (status) -> Void in
-        if status == PHAuthorizationStatus.Authorized {
-          allowed()
+    //  let assetsLibrary = ALAssetsLibrary()
+    var photoAssets: PHFetchResult?
+    var images: [UIImage] = [UIImage]()
+    
+    init() {
+        authorizeForAccess { () -> Void in
+            //success
+            self.getAllImagesFromGallery()
+            self.getAlbums()
+            
         }
-      })
-    } else if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.Authorized {
-      allowed()
     }
-  }
-
-  func getAllImagesFromGallery() {
-    photoAssets = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: nil)
-    let imageManager = PHCachingImageManager()
-    photoAssets?.enumerateObjectsUsingBlock({ (object, count, unsafePointer) -> Void in
-      if object is PHAsset {
-        let imageAsset = object as! PHAsset
-        let imageSize = CGSize(width: imageAsset.pixelWidth, height: imageAsset.pixelHeight)
-        let options = PHImageRequestOptions()
-        options.deliveryMode = PHImageRequestOptionsDeliveryMode.FastFormat
-
-        imageManager.requestImageForAsset(imageAsset, targetSize: imageSize, contentMode: PHImageContentMode.AspectFit, options: options, resultHandler: { (image, info) -> Void in
-          self.images.append(image!)
+    
+    func authorizeForAccess(allowed:() -> Void) {
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.NotDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                if status == PHAuthorizationStatus.Authorized {
+                    allowed()
+                }
+            })
+        } else if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.Authorized {
+            allowed()
+        }
+    }
+    
+    func getAllImagesFromGallery() {
+        photoAssets = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: nil)
+        let imageManager = PHCachingImageManager()
+        photoAssets?.enumerateObjectsUsingBlock({ (object, count, unsafePointer) -> Void in
+            if object is PHAsset {
+                let imageAsset = object as! PHAsset
+                let imageSize = CGSize(width: imageAsset.pixelWidth, height: imageAsset.pixelHeight)
+                let options = PHImageRequestOptions()
+                options.deliveryMode = PHImageRequestOptionsDeliveryMode.FastFormat
+                
+                imageManager.requestImageForAsset(imageAsset, targetSize: imageSize, contentMode: PHImageContentMode.AspectFit, options: options, resultHandler: { (image, info) -> Void in
+                    self.images.append(image!)
+                })
+            }
         })
-      }
-    })
-  }
+    }
+    
+    func getAlbums() {
+        
+        let albums = PHAssetCollection.fetchAssetCollectionsWithType(.Album, subtype: .Any, options: nil)
+        let aa = albums.objectAtIndex(0) as! PHAssetCollection
+        print(aa.localizedTitle)
+    }
+    
+    class func getHighQualityImage(imageAssets: AnyObject, getImage:(image: UIImage) -> Void) {
+        var assets = PHAsset()
+        if let asset = imageAssets as? PHAsset {
+            assets = asset
+        } else {
+            return
+        }
+        
+        let imageSize = CGSize(width: imageAssets.pixelWidth, height: imageAssets.pixelHeight)
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .HighQualityFormat
+        PHCachingImageManager().requestImageForAsset(assets, targetSize: imageSize, contentMode: .AspectFit, options: options) { (image, info) -> Void in
+            if let image = image {
+                getImage(image: image)
+            }
+            
+        }
+        
+        
+    }
 }
--- photoPickerView/GalleryViewController.swift
+++ photoPickerView/GalleryViewController.swift
@@ -9,42 +9,52 @@
 import UIKit
 
 class GalleryViewController: UIViewController {
-
-  @IBOutlet weak var galleryCollectionView: UICollectionView!
-  var photosData: GalleryDataSource = GalleryDataSource()
+    let NUMBER_OF_CELLS_IN_ROW: CGFloat = 5
+    let interSpacingBetweenItem: CGFloat = 5
+    @IBOutlet weak var galleryCollectionView: UICollectionView!
+    var photosData: GalleryDataSource = GalleryDataSource()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-
-    galleryCollectionView.delegate = self
-    galleryCollectionView.dataSource = self
-    galleryCollectionView.registerNib(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: "cell")
-  }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        galleryCollectionView.delegate = self
+        galleryCollectionView.dataSource = self
+        galleryCollectionView.registerNib(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+    }
+    
 }
 
 
 extension GalleryViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
-  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = galleryCollectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! PhotoCell
-    cell.photoImageView.image = photosData.images[indexPath.item]
-    return cell
-  }
-
-  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return photosData.images.count
-  }
-
-  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-    return CGSize(width: 90, height: 90)
-  }
-
-  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-    print(indexPath.item)
-  }
-
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = galleryCollectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! PhotoCell
+        cell.photoImageView.image = photosData.images[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photosData.images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let size = (galleryCollectionView.bounds.width - (NUMBER_OF_CELLS_IN_ROW  * interSpacingBetweenItem)) / NUMBER_OF_CELLS_IN_ROW
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return interSpacingBetweenItem
+    }
+    
+    func imageSelected(image: UIImage) {
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let imageAssets = photosData.photoAssets?.objectAtIndex(indexPath.item)
+        GalleryDataSource.getHighQualityImage(imageAssets!, getImage: imageSelected)
+    }
+    
 }
